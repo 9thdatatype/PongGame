8f69b976789aeabadf1b178b6a1b554b377b080a
9thdatatype@gmail.com <9thdatatype@gmail.com>
2016-02-03 11:56:24 -0500

    Did some updating to the PongGUI.java to draw objects

--- JavaFiles/PongGUI.java
+++ JavaFiles/PongGUI.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -11,41 +12,57 @@ import javax.swing.JFrame;
 public class PongGUI {
 
 	public static int MAX_FPS = 60;
-	
+
 	public int width = 960;
 	public int height = 720;
-	
+
 	PongGUI(){}
-	
+
 	PongGUI(int width, int height){
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public void createAndShowGUI(){
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(width, height);
 		jframe.setResizable(false);
 		jframe.setVisible(true);
 	}
-	
+
 	public void run(){
 		g2d = (Graphics2D) jframe.getGraphics();
-		
-		BufferedImage img = null;
-		
+
+		BufferedImage img = new BufferedImage(jframe.getWidth(), jframe.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		BufferedImage background = new BufferedImage(jframe.getWidth(), jframe.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
 		long startTime, endTime, timeTaken;
-		double frameTime = 1000/60;
+		double frameTime = 1000/MAX_FPS;
+
+		int x = 50, y = 50;
+		
+		for(int i = 0; i < jframe.getWidth(); ++i)
+			for(int j = 0; j < jframe.getHeight(); ++j)
+				background.setRGB(i, j, Color.white.getRGB());
 		
 		while(running){
 			startTime = System.currentTimeMillis();
 			
-			img = new BufferedImage(jframe.getWidth(), jframe.getHeight(), BufferedImage.TYPE_INT_ARGB);
+			img.setData(background.getRaster());
+			
+			int array[] = new int[50*50];
+			
+			for(int i = 0; i < array.length; ++i)
+				array[i] = Color.black.getRGB();
+			
+			img.setRGB(x, y, 50, 50, array, 0, 0);
 			
 			g2d.drawImage(img, 0, 0, this.jframe);
 			
-			endTime = System.currentTimeMillis();
+			x++; y++;
 			
+			endTime = System.currentTimeMillis();
+
 			timeTaken = endTime - startTime;
 			System.out.println(timeTaken);
 			if(timeTaken < frameTime)
@@ -56,19 +73,19 @@ public class PongGUI {
 				}	
 		}
 	}
-	
+
 	/*
 	 * Private portion of code
 	 */
-	
+
 	//Visual elements
-	
+
 	private JFrame jframe = new JFrame();
 	private Graphics2D g2d;
-	
+
 	// Game Variables
-	
+
 	private boolean running = true;
-	
+
 }
 // end class
