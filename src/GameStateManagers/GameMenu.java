fa5207ae0b8edc128669bf12532fe01634138fdb
Dan Thertell <dthertell@gmail.com>
2016-02-08 13:44:34 -0500

    a

--- src/GameStateManagers/GameMenu.java
+++ src/GameStateManagers/GameMenu.java
@@ -5,11 +5,13 @@ package GameStateManagers;
 
 import gameObject.GameObject;
 
+
 /**
  * @author Daniel Thertell
  * @version 0.1
  * @since Feb 8, 2016
  */
+
 public class GameMenu {
 	GameObject[] menuItems = null;
 	
