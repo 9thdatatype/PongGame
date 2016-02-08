de547549662c806bd17e194796c14becbd3f52fb
Dan Thertell <dthertell@gmail.com>
2016-02-08 13:39:06 -0500

    added GameMenu

---
+++ src/GameStateManagers/GameMenu.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package GameStateManagers;
+
+import gameObject.GameObject;
+
+/**
+ * @author Daniel Thertell
+ * @version 0.1
+ * @since Feb 8, 2016
+ */
+public class GameMenu {
+	GameObject[] menuItems = null;
+	
+}
