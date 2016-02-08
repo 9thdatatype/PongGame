bcb997fe91e25601da59d9f52c1efad12e2034d8
9thdatatype@gmail.com <9thdatatype@gmail.com>
2016-02-08 14:27:14 -0500

    Added a few txt files

--- src/GameStateManagers/StartUpManager.java
+++ src/GameStateManagers/StartUpManager.java
@@ -1,5 +1,37 @@
 package GameStateManagers;
 
-public class StartUpManager {
+import java.io.File;
+import fileManagement.GenericFileIO;
 
+public class StartUpManager {
+	
+	public StartUpManager(String configFilePath){
+		if(!new File(configFilePath).exists())
+			return;
+		
+		configFile = new File(configFilePath);
+	}
+	
+	public StartUpManager(File configFile){
+		if(!configFile.exists())
+			return;
+		
+		this.configFile = configFile;
+	}
+	
+	public String loadSettings(){
+		String returnMe = null;
+		String configSettings[] = new GenericFileIO(new File("\\resources\\PongConfig.txt")).readEntireFile();
+		
+		if(configSettings == null)
+			return null;
+		
+		for(String string:configSettings)
+			returnMe = string + "\n";
+		
+		return returnMe;
+	}
+	
+	private File configFile;
+	
 }
