e85a4bc25e9a3145c59029f8a64996168060e4a8
9thdatatype@gmail.com <9thdatatype@gmail.com>
2016-03-09 10:40:24 -0500

    Changed object order

--- src/GameStateManagers/InGameManager.java
+++ src/GameStateManagers/InGameManager.java
@@ -31,6 +31,7 @@ public class InGameManager {
 
 		ArrayList<GameObject> objs = new ArrayList<GameObject>();
 
+		objs.add(new GameObject(new Point(0, 0), new Point(width, height), "resources/pongCourt.png", null));
 		objs.add(new GameObject(new Point(50, 50), new Point(75, 75), "resources\\ball.png", null));
 		objs.add(new GameObject(new Point(50, height/2), 50, 200, "resources\\paddle.png", null));
 		objs.add(new GameObject(new Point(width-100, height/2), 50, 200, "resources\\paddle.png", null));
@@ -38,42 +39,41 @@ public class InGameManager {
 		objs.add(new GameObject(new Point((width/2), height-1), width, 1, "resources/border2.png", null));
 		objs.add(new GameObject(new Point(0, (height/2)), 1, height, "resources/border1.png", null));
 		objs.add(new GameObject(new Point(width-1, (height/2)), 1, height, "resources/border1.png", null));
-		objs.add(new GameObject(new Point(0, 0), new Point(width, height), "resources/pongCourt.png", null));
 		
-		objs.get(0).setName("Ball");
-		objs.get(1).setName("Paddle 1");
-		objs.get(2).setName("Paddle 2");
-		objs.get(3).setName("Side 1");
-		objs.get(4).setName("Side 2");
-		objs.get(5).setName("Side 3");
-		objs.get(6).setName("Side 4");
+		objs.get(1).setName("Ball");
+		objs.get(2).setName("Paddle 1");
+		objs.get(3).setName("Paddle 2");
+		objs.get(4).setName("Side 1");
+		objs.get(5).setName("Side 2");
+		objs.get(6).setName("Side 3");
+		objs.get(7).setName("Side 4");
 		
-		objs.get(1).setDirection(-90);
-		System.out.println("Paddle 1 Dir: " + objs.get(1).getPhys().direction);
 		objs.get(2).setDirection(-90);
-		System.out.println("Paddle 2 Dir: " + objs.get(2).getPhys().direction);
-		objs.get(0).setDirection(20);
-		objs.get(0).setSpeed(ballSpeed);
+		System.out.println("Paddle 1 Dir: " + objs.get(2).getPhys().direction);
+		objs.get(3).setDirection(-90);
+		System.out.println("Paddle 2 Dir: " + objs.get(3).getPhys().direction);
+		objs.get(1).setDirection(20);
+		objs.get(1).setSpeed(ballSpeed);
 		//TODO: Add the physics engine in here
 		//A Change
 		//Main game loop
 		while(running){
 			startTime = System.currentTimeMillis();
-			objs.get(1).setSpeed(0);
 			objs.get(2).setSpeed(0);
+			objs.get(3).setSpeed(0);
 			if(input.getInput().contains("w")){
 //				objs.get(0).getBottomRightCorner().translate(1, 1);
 //				objs.get(0).getCenter().translate(1, 1);
 //				objs.get(0).setCenter(new Point(objs.get(0).getCenter().x+1, objs.get(0).getCenter().y+1));
-				objs.get(1).setSpeed(speed);
+				objs.get(2).setSpeed(speed);
 			}else if(input.getInput().contains("s")){
-				objs.get(1).setSpeed(-speed);
+				objs.get(2).setSpeed(-speed);
 			}
 				
 			if(input.getInput().contains("i")){
-				objs.get(2).setSpeed(speed);
+				objs.get(3).setSpeed(speed);
 			}else if(input.getInput().contains("k")){
-				objs.get(2).setSpeed(-speed);
+				objs.get(3).setSpeed(-speed);
 			}
 			
 			
@@ -106,35 +106,34 @@ public class InGameManager {
 						if(crash.getName().equals("Paddle 1")){
 							//System.out.println("Ball hit Left Paddle");
 							ballSpeed++;
-							objs.get(0).bounceHorizontal();
-							objs.get(0).setSpeed(ballSpeed);
+							objs.get(1).bounceHorizontal();
+							objs.get(1).setSpeed(ballSpeed);
 						}else if(crash.getName().equals("Paddle 2")){
 							//System.out.println("Ball hit Right Paddle");
 							ballSpeed++;
-							objs.get(0).bounceHorizontal();
-							objs.get(0).setSpeed(ballSpeed);
+							objs.get(1).bounceHorizontal();
+							objs.get(1).setSpeed(ballSpeed);
 						}else if(crash.getName().equals("Side 1")){
-							objs.get(0).bounceVertical();
+							objs.get(1).bounceVertical();
 							//System.out.println("Ball hit Top");
 						}else if(crash.getName().equals("Side 2")){
-							objs.get(0).bounceVertical();
+							objs.get(1).bounceVertical();
 							//System.out.println("Ball hit Bottom!");
 						}else if(crash.getName().equals("Side 3")){
 							p2s++;
-							objs.get(0).setCenter(new Point(width/2,height/2));
+							objs.get(1).setCenter(new Point(width/2,height/2));
 							ballSpeed = 10;
-							objs.get(0).bounceHorizontal();
-							objs.get(0).setSpeed(ballSpeed);
+							objs.get(1).bounceHorizontal();
+							objs.get(1).setSpeed(ballSpeed);
 						}else if(crash.getName().equals("Side 4")){
 							p1s++;
-							objs.get(0).setCenter(new Point(width/2,height/2));
+							objs.get(1).setCenter(new Point(width/2,height/2));
 							ballSpeed = 10;
-							objs.get(0).bounceHorizontal();
-							objs.get(0).setSpeed(ballSpeed);
+							objs.get(1).bounceHorizontal();
+							objs.get(1).setSpeed(ballSpeed);
 						}
 					}
 				}//BALL COLLISION DONE
-				
 			}
 			
 			endTime = System.currentTimeMillis();
